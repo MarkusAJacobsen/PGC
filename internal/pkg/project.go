8806d321cf12594da392c4811cbd91a690be8811
MarkusAJacobsen <markus.jacobsen@marcello.no>
2019-05-20 19:42:30 +0200

    PGC-22 go fmt

--- internal/pkg/project.go
+++ internal/pkg/project.go
@@ -1,11 +1,11 @@
 package pkg
 
 type Project struct {
-	Id        string    `json:"id"`
-	Name      string    `json:"name"`
+	Id        string `json:"id"`
+	Name      string `json:"name"`
 	StartDate string `json:"startDate,omitempty"`
-	Climate   string    `json:"climate"`
-	Status    int64     `json:"status"`
+	Climate   string `json:"climate"`
+	Status    int64  `json:"status"`
 }
 
 type ProjectLink struct {
